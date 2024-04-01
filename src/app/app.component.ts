8c18e575c9b8a4353f3f40d9ad0c2d872cac7800
S. Koutelopoulos <spiros.koutelopoulos@gmail.com>
2024-04-01 17:38:15 +0300

    new component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,15 @@
 import { Component } from '@angular/core';
+import { PersonTableComponent } from './components/person-table/person-table.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [PersonTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   name = 'Spiros';
 
-  person = {
-    giveName: "Spiros",
-    surName: "Koutelopoulos",
-    age: 38,
-    email: "spiros.koutelopoulos@gmail.com"
-  };
+  
 }
